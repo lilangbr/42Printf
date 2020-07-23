c3f241b24f0bf1b26902d1577c9c5857d763cedf
Eliane Bresser lang <ebresser@c1r2p2.42sp.org.br>
2020-07-23 18:11:50 -0300

    norme fixed

--- ft_fillflags.c
+++ ft_fillflags.c
@@ -6,32 +6,32 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/06/09 15:56:56 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/23 18:10:56 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void     ft_fillflags(const char *fmt, int *fmt_inc, struct fields *f)
+void	ft_fillflags(const char *fmt, int *fmt_inc, struct fields *f)
 {
-    int count_minus;
-    int count_zero;
-    count_minus = 0;
-    count_zero = 0;
+	int count_minus;
+	int count_zero;
 
-    while(*fmt == '-' || *fmt == '0')
-    {
-        if(*fmt == '-' && count_minus == 0)
-        {
-            f->flagminus = 1;
-            count_minus++;
-        }
-        if(*fmt == '0' && count_zero == 0)
-        {
-            f->flagzero = 1;
-            count_zero++;
-        }
-        fmt++;
-        (*fmt_inc)++;
-    }
+	count_minus = 0;
+	count_zero = 0;
+	while (*fmt == '-' || *fmt == '0')
+	{
+		if (*fmt == '-' && count_minus == 0)
+		{
+			f->flagminus = 1;
+			count_minus++;
+		}
+		if (*fmt == '0' && count_zero == 0)
+		{
+			f->flagzero = 1;
+			count_zero++;
+		}
+		fmt++;
+		(*fmt_inc)++;
+	}
 }

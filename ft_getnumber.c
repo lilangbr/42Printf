15a226b4599e99777976f837adab710901dbe33b
Eliane Bresser lang <ebresser@c1r2p2.42sp.org.br>
2020-07-23 20:30:14 -0300

    norme fixed

--- ft_getnumber.c
+++ ft_getnumber.c
@@ -1,34 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_getnumber.c                                        :+:      :+:    :+:   */
+/*   ft_getnumber.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/06/09 15:56:56 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/23 20:29:12 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int      ft_getnumber(const char *fmt, int *fmt_inc)
+int		ft_getnumber(const char *fmt, int *fmt_inc)
 {
-    int number;
+	int number;
 
-    number = 0;
-    if(*fmt < 58 && *fmt > 47)
-    {
-        number = *fmt - 48;
-        fmt++;
-        (*fmt_inc)++;
-        while(*fmt < 58 && *fmt > 47)
-        {
-            number = number*10 + (*fmt - 48);
-            fmt++;
-            (*fmt_inc)++;
-        }
-    }
-            
-    return (number);
+	number = 0;
+	if (*fmt < 58 && *fmt > 47)
+	{
+		number = *fmt - 48;
+		fmt++;
+		(*fmt_inc)++;
+		while (*fmt < 58 && *fmt > 47)
+		{
+			number = number * 10 + (*fmt - 48);
+			fmt++;
+			(*fmt_inc)++;
+		}
+	}
+	return (number);
 }

4c32d88ca82d81fc415e8955f6bda51276e2b518
Eliane Bresser lang <ebresser@c1r2p2.42sp.org.br>
2020-07-23 18:29:56 -0300

    norme adjustments

--- ft_fillwidth.c
+++ ft_fillwidth.c
@@ -6,49 +6,45 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/06/09 15:56:56 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/23 18:29:13 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void     ft_fillwidth(va_list *p_ap, const char *fmt,int *fmt_inc,struct fields *f)
+void	ft_fillwidth(va_list *p_ap, const char *fmt, int *fmt_inc, \
+		struct fields *f)
 {
-    if(*fmt == '.')
-    {
-        f->point = 1;
-        f->width = 0;
-        fmt++;
-        (*fmt_inc)++;
-    }
-    else
-    {
-        if (*fmt == '*')
-        {
-            f->width = va_arg(*p_ap, int);
-            fmt++;
-//            printf("\n***********width = %d\n", f->width);
-//            printf("\n***********fmt = %c\n", *fmt);
-            (*fmt_inc)++;
-        }
-        else
-        {
-            f->width = ft_getnumber(fmt,fmt_inc);
-            fmt = fmt + *fmt_inc;
-//            printf("\n***********width = %d\n", f->width);
-//            printf("\n-----------fmt = %c\n", *fmt);
-        }
-        if(*fmt == '.')
-        {   
-            f->point = 1;
-            (*fmt_inc)++;
-        }
-        
-    }
-    if(f->width < 0) //**************************************16julho
-    {
-        f->flagminus = 1;
-        f->flagzero = 0;
-        f->width = - f->width;
-    }
+	if (*fmt == '.')
+	{
+		f->point = 1;
+		f->width = 0;
+		fmt++;
+		(*fmt_inc)++;
+	}
+	else
+	{
+		if (*fmt == '*')
+		{
+			f->width = va_arg(*p_ap, int);
+			fmt++;
+			(*fmt_inc)++;
+		}
+		else
+		{
+			f->width = ft_getnumber(fmt, fmt_inc);
+			fmt = fmt + *fmt_inc;
+		}
+		if (*fmt == '.')
+		{
+			f->point = 1;
+			(*fmt_inc)++;
+		}
+	}
+	if (f->width < 0)
+	{
+		f->flagminus = 1;
+		f->flagzero = 0;
+		f->width = -f->width;
+	}
 }

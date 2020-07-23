9eed0e9c7badd326659fd6cca16552b927ba47b6
Eliane Bresser lang <ebresser@c1r2p2.42sp.org.br>
2020-07-23 18:21:10 -0300

    norme adjustments

--- ft_fillprecision.c
+++ ft_fillprecision.c
@@ -6,42 +6,27 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/06/09 15:56:56 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/23 18:19:32 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-/*
- *
-static int mod(int n)
+int		ft_fillprecision(va_list *p_ap, const char *fmt, int *fmt_inc, struct fields *f)
 {
-    if(n < 0)
-        return(-n);
-    else
-        return(n);
-}
-*
-*/
-
-int      ft_fillprecision(va_list *p_ap, const char *fmt, int *fmt_inc, struct fields *f)
-{
-    if(*fmt == '-')
-        return (-1);
-    else if (f->point)
-    {
-        if(*fmt == '*')
-        {
-            f->precision = va_arg(*p_ap, int);
-            (*fmt_inc)++;
-         //   printf("\nPRECISAO: %d\n", f->precision);
-        }
-        else
-            f->precision = ft_getnumber(fmt,fmt_inc);
-     //   if(f->precision < 0)
-     //       f->precision = - f->precision; //***********
-        return (0);
-    }
-    else
-        return (0);
+	if (*fmt == '-')
+		return (-1);
+	else if (f->point)
+	{
+		if (*fmt == '*')
+		{
+			f->precision = va_arg(*p_ap, int);
+			(*fmt_inc)++;
+		}
+		else
+			f->precision = ft_getnumber(fmt, fmt_inc);
+		return (0);
+	}
+	else
+		return (0);
 }

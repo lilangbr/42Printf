625fa522eb33d9a78b49d273b090fd6b2c582701
Eliane Bresser lang <ebresser@c1r2p2.42sp.org.br>
2020-07-24 00:32:18 -0300

    norme fixed

--- ft_specifier_redirect.c
+++ ft_specifier_redirect.c
@@ -6,7 +6,7 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/06/09 15:56:56 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/24 00:31:51 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 void	ft_specifier_redirect(va_list *p_ap, char sp, int *p, t_fields *f)
 {
 	if (sp == 'd' || sp == 'i')
-		ft_printint(p_ap, 1, p, f); 
+		ft_printint(p_ap, 1, p, f);
 	else if (sp == 'u')
 		ft_printint(p_ap, 0, p, f);
 	else if (sp == 'c')
 		ft_printchar(p_ap, p, f);
 	else if (sp == 's')
-		ft_printstr(p_ap, p, f); 
+		ft_printstr(p_ap, p, f);
 	else if (sp == 'x')
 		ft_printhex(p_ap, 0, p, f);
 	else if (sp == 'X')

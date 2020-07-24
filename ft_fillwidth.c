bc44feae4b3d3b008878b60387f974b58ecfcaee
lilangbr <eblang@gmail.com>
2020-07-24 19:04:06 -0300

    norme adjustments

--- ft_fillwidth.c
+++ ft_fillwidth.c
@@ -12,16 +12,33 @@
 
 #include "ft_printf.h"
 
+static void aux(int mode, t_fields *f, int *fmt_inc)
+{
+		if (mode == 1)
+		{
+			f->point = 1;
+			f->width = 0;
+			(*fmt_inc)++;
+		}
+		else if (mode == 2)
+		{
+			f->point = 1;
+			(*fmt_inc)++;
+		}
+		else if (mode == 3)
+		{
+			f->flagminus = 1;
+			f->flagzero = 0;
+			f->width = -f->width;
+		}
+
+}
+
 void	ft_fillwidth(va_list *p_ap, const char *fmt, int *fmt_inc, \
 		t_fields *f)
 {
 	if (*fmt == '.')
-	{
-		f->point = 1;
-		f->width = 0;
-		fmt++;
-		(*fmt_inc)++;
-	}
+		aux(1, f, fmt_inc);
 	else
 	{
 		if (*fmt == '*')
@@ -36,15 +53,8 @@ void	ft_fillwidth(va_list *p_ap, const char *fmt, int *fmt_inc, \
 			fmt = fmt + *fmt_inc;
 		}
 		if (*fmt == '.')
-		{
-			f->point = 1;
-			(*fmt_inc)++;
-		}
+			aux(2, f, fmt_inc);
 	}
 	if (f->width < 0)
-	{
-		f->flagminus = 1;
-		f->flagzero = 0;
-		f->width = -f->width;
-	}
+		aux(3, f, fmt_inc);
 }

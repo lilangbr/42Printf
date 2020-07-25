19fa7c527c19c6b20d41d5b20e0d198659ffcd00
lilangbr <eblang@gmail.com>
2020-07-25 14:02:22 -0300

    norme adjustments

--- ft_printf.c
+++ ft_printf.c
@@ -12,28 +12,27 @@
 
 #include "ft_printf.h"
 
-static int	aux(int mode, const char **fmt2, int *p, t_fields *f, int *fmt_inc, va_list *p_ap)
+static int	aux1(const char **fmt2, int *p)
 {
-	if (mode == 1)
-	{
-		ft_putchar(**fmt2, p);
-		(*fmt2)++;
-		return (0);
-	}
-	else if (mode == 2)
-	{
-		ft_strformat_init(f);
-		if (ft_fieldstorage(p_ap, *fmt2, fmt_inc, f) == -1)
-		{
-			free(f);
-			return (-1);
-		}
-		ft_specifier_redirect(p_ap, f->specifier, p, f);
-		*fmt2 = *fmt2 + *fmt_inc;
-		return (0);
-	}
+	ft_putchar(**fmt2, p);
+	(*fmt2)++;
 	return (0);
 }
+
+static int	aux2(const char **fmt2, int *p, t_fields *f,\
+			int *fmt_inc, va_list *p_ap)
+{
+	ft_strformat_init(f);
+	if (ft_fieldstorage(p_ap, *fmt2, fmt_inc, f) == -1)
+	{
+		free(f);
+		return (-1);
+	}
+	ft_specifier_redirect(p_ap, f->specifier, p, f);
+	*fmt2 = *fmt2 + *fmt_inc;
+	return (0);
+}
+
 int			ft_printf(const char *fmt, ...)
 {
 	int			printed;
@@ -48,14 +47,14 @@ int			ft_printf(const char *fmt, ...)
 	while (*fmt)
 	{
 		if (*fmt != '%')
-			aux(1, &fmt, &printed, strformat, &fmt_inc, &ap);
+			aux1(&fmt, &printed);
 		else
 		{
 			fmt++;
 			if (*fmt == '%')
-				aux(1, &fmt, &printed, strformat, &fmt_inc, &ap);
-			else if (aux(2, &fmt, &printed, strformat, &fmt_inc, &ap) == -1)
-				return(-1);
+				aux1(&fmt, &printed);
+			else if (aux2(&fmt, &printed, strformat, &fmt_inc, &ap) == -1)
+				return (-1);
 		}
 	}
 	va_end(ap);

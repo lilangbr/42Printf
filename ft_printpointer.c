24ff62f6d3622edd04f1e404187aa55714cbfa20
Eliane Bresser lang <ebresser@c1r1p4.42sp.org.br>
2020-07-27 00:53:10 -0300

    norme fixed

--- ft_printpointer.c
+++ ft_printpointer.c
@@ -6,7 +6,7 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/07/24 00:11:38 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/27 00:50:05 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ static void	aux_noprecision(t_fields *f, size_t add, int add_len)
 	}
 }
 
-static void aux_precision(t_fields *f, size_t add, int add_len)
+static void	aux_precision(t_fields *f, size_t add, int add_len)
 {
 	int zero;
 	int space;
@@ -61,55 +61,13 @@ void		ft_printpointer(t_fields *f)
 {
 	int		add_len;
 	size_t	add;
-	//int		space;
-	//int		zero;
 
 	add = (size_t)va_arg(f->ap, void *);
 	add_len = ft_len_add(add) + 2;
 	if (!f->precision && !add && f->point)
 		add_len = 2;
 	if (f->point && (f->precision >= 0))
-	{
 		aux_precision(f, add, add_len);
-		/*
-		zero = ft_min(f->precision) - len_add(add);
-		if (zero > 0)
-			space = f->width - f->precision;
-		else
-			space = f->width - add_len;
-		if (!f->flagminus)
-			ft_printspacezero(1, space, &(f->printed));
-		ft_putstr("0x", &(f->printed), 2);
-		if (zero > 0)
-			ft_printspacezero(0, zero, &(f->printed));
-		if (f->precision != 0)
-			ft_putnbr_hex(add, 0, &(f->printed));
-		if (f->flagminus)
-			ft_printspacezero(1, space, &(f->printed));
-			*/
-	}
 	else
-	{
 		aux_noprecision(f, add, add_len);
-		/*
-		space = f->width - add_len;
-		zero = space;
-		if (f->flagzero && !f->flagminus)
-		{
-			ft_printspacezero(0, space, &(f->printed));
-			ft_putstr("0x", &(f->printed), 2);
-			if (len_add(add))
-				ft_putnbr_hex(add, 0, &(f->printed));
-		}
-		else
-		{
-			if (!f->flagminus)
-				ft_printspacezero(1, space, &(f->printed));
-			ft_putstr("0x", &(f->printed), 2);
-			if (len_add(add))
-				ft_putnbr_hex(add, 0, &(f->printed));
-			if (f->flagminus)
-				ft_printspacezero(1, space, &(f->printed));
-		}*/
-	}
 }

333c0ba1387a2c4a75bba411e8e7ec2cd3b07d90
Eliane Bresser lang <ebresser@c1r1p4.42sp.org.br>
2020-07-27 01:00:16 -0300

    norme fixed

--- ft_printhex.c
+++ ft_printhex.c
@@ -6,13 +6,13 @@
 /*   By: ebresser <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/09 11:00:03 by ebresser          #+#    #+#             */
-/*   Updated: 2020/07/23 23:39:39 by ebresser         ###   ########.fr       */
+/*   Updated: 2020/07/27 00:56:33 by ebresser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void aux_precision(t_fields *f, int h_len, int cap, unsigned int h)
+static void	aux_precision(t_fields *f, int h_len, int cap, unsigned int h)
 {
 	int space;
 	int zero;
@@ -58,53 +58,13 @@ void		ft_printhex(int capitalized, t_fields *f)
 {
 	unsigned int	h;
 	int				h_len;
-	//int				space;
-	//int				zero;
 
 	h = va_arg(f->ap, unsigned int);
 	h_len = ft_len_hex(h);
 	if (!f->precision && !h && f->point)
 		h_len = 0;
 	if (f->point && (f->precision >= 0))
-	{
 		aux_precision(f, h_len, capitalized, h);
-		/*
-		 * zero = f->precision - h_len;
-		if (zero > 0)
-			space = f->width - f->precision;
-		else
-			space = f->width - h_len;
-		if (!(f->flagminus))
-		{
-			ft_printspacezero(1, space, &(f->printed));
-			if (zero > 0)
-				ft_printspacezero(0, zero, &(f->printed));
-			if (h_len)
-				ft_putnbr_hex(h, capitalized, &(f->printed));
-		}
-		if (f->flagminus)
-			ft_printspacezero(1, space, &(f->printed));
-			*/
-	}
 	else
-	{
 		aux_noprecision(f, h_len, capitalized, h);
-		/*
-		space = f->width - h_len;
-		if (f->flagzero && !f->flagminus)
-		{
-			ft_printspacezero(0, space, &(f->printed));
-			if (h_len)
-				ft_putnbr_hex(h, capitalized, &(f->printed));
-		}
-		else
-		{
-			if (!f->flagminus)
-				ft_printspacezero(1, space, &(f->printed));
-			if (h_len)
-				ft_putnbr_hex(h, capitalized, &(f->printed));
-			if (f->flagminus)
-				ft_printspacezero(1, space, &(f->printed));
-		}*/
-	}
 }
